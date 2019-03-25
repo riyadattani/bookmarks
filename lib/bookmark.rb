ffc554e747a683cac1add07aceeb07e135e4de9f
saypop <tome.m.dj@gmail.com>
2019-03-25 18:04:12 +0000

    Refactored out comments

--- lib/bookmark.rb
+++ lib/bookmark.rb
@@ -2,19 +2,9 @@ require 'pg'
 
 class Bookmark
 
-  # @list = []
-  # conn = PG.connect( dbname: 'bookmark_manager')
-  # conn.exec( "SELECT * FROM bookmarks" ) do |result|
-  #   result.each do |row|
-  #     @list << row.values_at('url')[0]
-  #   end
-  #   @list
-  # end
-
   def self.all
     connection = PG.connect(dbname: 'bookmark_manager')
     result = connection.exec('SELECT * FROM bookmarks')
     result.map { |bookmark| bookmark['url'] }
-    # @list
   end
 end
