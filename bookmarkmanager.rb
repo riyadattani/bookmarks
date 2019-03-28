6ee493d74ed99bd6cd9fb31dad6f90ae00cc2ee2
Riya Dattani <riya_dattani@hotmail.com>
2019-03-28 22:03:25 +0000

    User can update a bookmark

--- bookmarkmanager.rb
+++ bookmarkmanager.rb
@@ -28,5 +28,15 @@ class BookmarkManager < Sinatra::Base
     redirect '/bookmarks'
   end
 
+  get '/bookmarks/:id/edit' do
+    @bookmark = Bookmark.find(id: params[:id])
+    erb :'bookmarks/edit'
+  end
+
+  patch '/bookmarks/:id' do
+    Bookmark.update(id: params[:id], url: params[:url], title: params[:title])
+    redirect '/bookmarks'
+  end
+
   run! if app_file == $0
 end
